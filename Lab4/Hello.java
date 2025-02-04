25262355cd49a0bab71bf78536ea647f8acc9af7
mesikes1027 <mesikes@umich.edu>
2025-02-04 17:35:30 -0500

    new branch

--- Lab4/Hello.java
+++ Lab4/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         for(int i=1;i<=12;i++) { // Changed loop from 10 to 12
             System.out.println("Hello World..."+i);
+            System.out.println("New Branch!");
         }
     }
 }
